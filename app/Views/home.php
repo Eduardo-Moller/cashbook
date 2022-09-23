14d9424f70043222f3ceda6c4cfb5d7db2c800c2
João Gabriel Schwabe <schwabejoao@gmail.com>
2022-09-23 09:08:58 -0300

    v1.0

---
+++ app/Views/home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to CodeIgniter 4!</title>
+    <meta name="description" content="The small framework with powerful features">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+
+    <!-- STYLES -->
+    <link href="<?php echo  base_url() ?>\public\assets\bootstrap/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+
+        <div class="menu">
+
+        </div>
+
+    </header>
+
+    <!-- CONTENT -->
+
+    <section>
+
+        <h1>sim</h1>
+
+    </section>
+
+    <div class="further">
+
+        <section>
+
+
+        </section>
+
+    </div>
+
+    <!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
+
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
